424b06882ca8893fa5607f5e3c0d02de6ca1b4e8
Pedro Teixeira <123015308+PedroTeixeira-dev@users.noreply.github.com>
2023-11-01 14:27:50 -0300

    "exercicio consuimindo a api do github"

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const profileAvatar = document.querySelector('#profile-avatar')
+    const profileName = document.querySelector('#profile-name')
+    const profileUserName = document.querySelector('#profile-username')
+    const repositorys = document.querySelector('#repositorys')
+    const followers = document.querySelector('#followers')
+    const following = document.querySelector('#following')
+    const profileLink = document.querySelector('.profile-link')
+    const urlAPI = 'https://api.github.com/users/PedroTeixeira-dev'
+
+    fetch(urlAPI)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(json){
+        profileAvatar.src = json.avatar_url
+        profileName.innerText = json.name 
+        profileUserName.innerText = json.login
+        repositorys.innerHTML = json.public_repos
+        followers.innerHTML = json.followers
+        following.innerHTML = json.following
+        profileLink.href = json.html_url
+    })
+})
